92126eb425645c5b4b68c72f5679fe0c809edcb3
anchoredinlord <damozemotumaguyasa@gmail.com>
2026-05-19 21:29:01 +0300

    fix: update official URL to damozemotuma.vercel.app

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://portfolio-livid-omega.vercel.app/sitemap.xml",
+    sitemap: "https://damozemotuma.vercel.app/sitemap.xml",
   };
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://portfolio-livid-omega.vercel.app",
+      url: "https://damozemotuma.vercel.app",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
